563cc79fc620859d3827c1aaabd4f9626d9883ce
ZAIN ALI SHAH <155235083+Zain-4321@users.noreply.github.com>
2024-08-18 23:45:48 +0500

    Q01 to Q04

---
+++ script .js
@@ -0,0 +1,88 @@
+
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. 
+//Your output should look something like the following, including the quotation marks: Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+// console.log(`Albert Einstein once said, “A person who never made a mistake never tried anything new.”`);
+
+// Personal Message: Store a person’s name in a variable, and print a message to that person. 
+// Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+// let name="Javascript";
+// console.log(`today i star my new journay in  ${name} world`);
+
+// Name Cases: Store a person’s name in a variable, and then print that person’s
+//  name in lowercase, uppercase, and titlecase.
+
+//touppercase
+// let myName="zain ali shah";
+// console.log(myName.toUpperCase());
+
+
+//tolowercase
+// let myName="ZAIN ALI SHAH";
+// console.log(myName.toLocaleLowerCase());
+
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s 
+// name in a variable called famous_person. Then compose your message and store it in a new variable called message.
+//  Print your message.
+
+
+// let famous_person="Albert Einstein";
+// console.log(`${famous_person} once said, “A person who never made a mistake never tried anything new.`);
+
+// Stripping Names: Store a person’s name, and include some whitespace characters at the beginning 
+// and end of the name. Make sure you use each character combination, " " and " ", at least once. 
+// Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+
+
+// let firstName="zain";
+// let lastName="Ali shah";
+// console.log(firstName + " " +lastName);
+
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each 
+// result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+//Additions
+// let num=5;
+// console.log(num+3);
+
+//sub
+// let num=11;
+// console.log(num-3);
+
+//multip
+// let num=2;
+// console.log(num*4);
+
+//Divi
+// let num=16;
+// console.log(num/2);
+
+
+// console.log(5+3);
+// console.log(5+3);
+
+// console.log(5+3);
+
+// console.log(5+3);
+
+// let favNum=69;
+// console.log(`My Favorite number is ${favNum} and this is my roll number in university`);
+
+// let arr=["tesla","toyota","bmw","audi"];
+// for(i=1;i<=arr.length;i++){
+//     console.log(`I own all this companies ${arr}`);
+// }
+
+
+
+// let fri=["talha","waqar","salman"]
+// for(i=0;i<fri.length;i++){
+//     console.log(fri[0]);
+// }
+
+
+let fri=["talha","waqar","salman"]
+for(let name in fri){
+    console.log(`hello ${fri} ,would you like a javascript`);
+}
+
+//*****************Q01*/ TO Q 04**************************
