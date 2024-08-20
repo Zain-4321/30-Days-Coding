370bd727fbfe32c9399845ef0688cbdf82fcdaf4
ZAIN ALI SHAH <155235083+Zain-4321@users.noreply.github.com>
2024-08-21 00:53:25 +0500

    DOM Introductions

---
+++ app.js
@@ -0,0 +1,24 @@
+//DOM full form document object model are linked with html 
+//DOM ek tree form hota hai jo hamre strucure ko zahir karta hai
+// DOM ek global object hai jo hum window ke under access karskte hain
+
+
+
+// how Access
+//getElemenById();
+const idd=document.getElementById("id");
+//console.log(idd);
+// document.getElementsByClassName();
+const clsss=document.getElementsByClassName("class");
+//console.log(clsss);
+// document.getElementsByName();
+// document.getElementsByTagName();
+const spann=document.getElementsByTagName("span")
+//console.log(spann);
+
+// document.querySelector();
+const ull=document.querySelector("li");
+console.log(ull);
+
+// document.querySelectorAll();
+
