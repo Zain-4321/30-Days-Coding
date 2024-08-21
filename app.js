60fd0334b3f008cd720aa8283862cc874f26c9d0
ZAIN ALI SHAH <155235083+Zain-4321@users.noreply.github.com>
2024-08-21 15:33:56 +0500

    Day 05 & 06 With ARRAYS

--- app.js
+++ app.js
@@ -1,24 +1,64 @@
-//DOM full form document object model are linked with html 
-//DOM ek tree form hota hai jo hamre strucure ko zahir karta hai
-// DOM ek global object hai jo hum window ke under access karskte hain
+
+// Your Own Array: Think of your favorite mode of transportation
+// , such as a motorcycle or a car, and make a list that stores several examples.
+//  Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+// let arr=["BmwBike" ,"TeslaCar","MercedesCar","HondaMotorcycle","ZR DOOM BIKE"]
+// console.log(`I would like to own a .${arr[3]}`);
+// for(let x in arr){
+//     console.log(`I would like to own a Honda motorcycle ${arr[x]}`);
+// }
 
 
 
-// how Access
-//getElemenById();
-const idd=document.getElementById("id");
-//console.log(idd);
-// document.getElementsByClassName();
-const clsss=document.getElementsByClassName("class");
-//console.log(clsss);
-// document.getElementsByName();
-// document.getElementsByTagName();
-const spann=document.getElementsByTagName("span")
-//console.log(spann);
+// G1uest List: If you could invite anyone, living or deceased, to dinner,
+//  who would you invite? Make a list that includes at least three people you’d like to invite to dinner. 
+//  Then use your list to print a message to each person, inviting them to dinner.
 
-// document.querySelector();
-const ull=document.querySelector("li");
-console.log(ull);
+// let arr=["Waqar ","Salman","Sufyan","Noman"];
+// console.log(`Hello ${arr} would you accept my Diner Today`)
+// for(let x in arr){
+//     console.log(`Hello ${arr} would you accept my Diner Today`);
+// }
 
-// document.querySelectorAll();
 
+// Changing Guest List: You just heard that one of your guests can’t make the dinner, 
+// so you need to send out a new set of invitations. You’ll have to think of someone else to invite, full-question
+
+//  let arr=["Waqar ","Salman","Sufyan","Noman"];
+// let newguest="Talha";
+// arr[arr.indexOf()]=newguest;
+//   console.log(`Hello ${newguest} would you accept my Diner Today`)
+
+              
+// let guests=["ZAIN","WAQAR","NOMAN","SALMAN","SUFYAN"]
+
+// guests.unshift("EJAZ");
+// guests.push("BILAL");
+
+// guests.forEach(guests =>{
+//     console.log(`Dear ${guests},would you liks joine me for Dinner Sorry ejaz and bilal are not invite`);
+
+// })
+// Shrinking Guest List: You just found out that your new dinner table won’t arrive in time
+//  for the dinner, and you have space for only two guests, full-question
+
+// let guests=["ZAIN","WAQAR","NOMAN","SALMAN","SUFYAN"]
+
+// guests.unshift("EJAZ");
+// guests.push("BILAL");
+// guests.pop();
+// guests.shift();
+// guests.forEach(guests =>{
+//     console.log(`Dear ${guests},would you liks joine me for Dinner Sorry ejaz and bilal are not invite`);
+
+// })
+
+
+// Seeing the World: Think of at least five places in the world you’d like to visit. - 
+// Store the locations in a array. Make sure the array is not in alphabetical order, full-question
+
+let places=["New York","London","Switzerland","Delhhi","Tokyo"]
+// // console.log(places);
+// // console.log(places.sort());
+// console.log(places.reverse());
