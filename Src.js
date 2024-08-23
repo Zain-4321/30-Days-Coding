1662b16498b1bcd8f573bf9c26bba1f503d191ff
ZAIN ALI SHAH <155235083+Zain-4321@users.noreply.github.com>
2024-08-23 16:39:57 +0500

    Day 09 If else conditions

---
+++ Src.js
@@ -0,0 +1,75 @@
+// Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color 
+// and assign it a value of green, yellow, or red. full-question
+
+// let alien_color="green";
+// if(alien_color=="green"){
+//     console.log("The conditions is true");
+// }else if(alien_color=="red"){
+//     console.log("The conditions is note true")
+// }
+
+
+
+// Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else
+//  chain. full-question
+
+// let alien_color="yellow";
+// if(alien_color=="yellow"){
+//     console.log("Your earned 10 dollars");
+// }else{
+//     console.log("You lost 10 dollars")
+// }
+
+// let Alien_color="green";
+// if(Alien_color=="red"){
+// console.log("You just earned 5 points for shooting the alien!");
+// } else {
+//   console.log("You just earned 10 points!");
+// }
+
+
+
+// Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain. - If the alien is green, 
+// print a message that the player earned 5 points, full-question
+
+// let Alien_color="pink";
+// if(Alien_color=="yellow")
+// {
+//   console.log("You just earned 10 points!");
+   
+// }else if(Alien_color=="green"){
+//      console.log("You just earned 05 points!");
+
+// }else{
+//     console.log("You are wrong");
+
+// }
+
+
+
+// let alien_color="pink";
+// if(alien_color=="yellow")
+// {
+//   console.log("You just earned 10 points!");
+   
+// }else if(alien_color=="pink"){
+//      console.log("You just earned 25 points!");
+
+// }else{
+//     console.log("You are wrong");
+
+// }
+
+// let Allien_color="red";
+// if(Allien_color=="red")
+// {
+//   console.log("You just earned 100 points!");
+   
+// }else if(Allien_color=="green"){
+//      console.log("You just earned 05 points!");
+
+// }else{
+//     console.log("You are wrong");
+
+// }
+
