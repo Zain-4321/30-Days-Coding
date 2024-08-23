96a216295c1ded4caa9b63a3789122a68a6ea663
ZAIN ALI SHAH <155235083+Zain-4321@users.noreply.github.com>
2024-08-23 15:40:40 +0500

    Day 07 Array And Object

---
+++ script.js
@@ -0,0 +1,29 @@
+//DAY 07
+
+
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating 
+// the number of people you are inviting to dinner.
+
+// let guestes=["Waqar","Salman","Talha","Noman","Bilal","Sufyan","Rafi","Khuzaima","Hanzala"]
+// console.log(`${guestes.length} I invite these friends in Dinner`);
+
+
+// Think of something you could store in a array. For example, you could make a list of
+//  mountains, rivers, countries, cities, languages, or anything else you’d like. 
+//  Write a program that creates a list containing these items.
+
+
+// let countries=["Pakistan","USA","England","Russia","India","Switzerland","Europe","Austrailia"]
+// console.log(`I want to tour in these countrirs ${countries}`);
+
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects
+//  containing these items.
+
+// let obj={
+//     title:"The Hobbite",
+//     Writer:"J.J Tolkien",
+//     published:1937,
+
+// }
+
+// console.log(`Novel Title Name ${obj.title}And his Writer Name ${obj.Writer} And their Published Date ${obj.published}`);
