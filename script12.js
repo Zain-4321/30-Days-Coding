0436c40b255e5425d55acec5e5f66db02268c2d5
ZAIN ALI SHAH <155235083+Zain-4321@users.noreply.github.com>
2024-08-25 15:47:58 +0500

    Day 12 Loop And one Q of Fuctions

---
+++ script12.js
@@ -0,0 +1,33 @@
+// Day 12
+// Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, 
+// and then use a for loop to print the name of each pizza. full-question
+
+// let arr=["chicken","Miyo","Garlic"]
+
+// for(i=0;i<arr.length;i++){
+//     console.log(`I really love pizza ${arr[i]}`);
+// }
+
+
+// Animals: Think of at least three different animals that have a common characteristic. Store the names of 
+// these animals in a list, and then use a for loop to print out the name of each animal. full-question
+
+// let animal=["Loin","Tiger","Cat","Dog"];
+// for(i=0;i<animal.length;i++){
+//     console.log(`This Animal Most popular in the world there name is ${animal[i]}`);
+// }
+
+
+// T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should 
+// be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the 
+// message printed on it. Call the function.
+
+// function shirt(size,message){
+// console.log(    `Making a ${size} t-shirt with the message "${message}" printed on it`
+// );
+
+
+// }
+
+// shirt("large","code is life")
+
