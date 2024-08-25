ac321d173ab384dc39816414857c65fcd6198837
ZAIN ALI SHAH <155235083+Zain-4321@users.noreply.github.com>
2024-08-25 16:03:47 +0500

    Only Use Functions

---
+++ Script13.js
@@ -0,0 +1,39 @@
+//Day 13 with javaScript
+
+// Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads
+// I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with
+//  a different message.
+
+// function shirt(size,message){
+
+//     console.log(`Making a ${size} t-shirt with the message 
+// ${message} printed on it.`);
+// }
+// shirt();
+// shirt("medium","I love JavaScript");
+// shirt("Small","It's your turn ");
+
+
+// Cities: Write a function called describe_city() that accepts the name of a city and its country. The function
+//  should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a default value. 
+//  Call your function for three different cities, at least one of which is not in the default country.
+
+// function Cities(city,country){
+//     console.log(`${city} is the city of ${country}`);
+// }
+
+// Cities("Karachi","Pakistan");
+// Cities("New York","USA");
+// Cities("Tokyo","Japan");
+
+
+// City Names: Write a function called city_country() that takes in the name of a city and its country. 
+// The function should return a string formatted like this full-question
+
+// function cities(city,country){
+//     return `${city} ${country}`
+// }
+
+// console.log(cities("Karachi","Pakistan"));
+// console.log(cities("Dehli","India"));
+// console.log(cities("London","England"));
