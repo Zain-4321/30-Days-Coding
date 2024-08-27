1aa9318dc31cc26bfbd08f56f30c9e6412d8316a
ZAIN ALI SHAH <155235083+Zain-4321@users.noreply.github.com>
2024-08-27 22:53:56 +0500

    Day 17   someFunctions  & 1 Arrow Question

---
+++ Script17.js
@@ -0,0 +1,35 @@
+//Day 17
+
+// Function with Rest Parameters: Write a function that takes a rest parameter representing multiple hobbies.
+//  It should log each hobby with a statement saying you enjoy that hobby.
+
+// rest parameter utni zyada argument deta hai jitne apko chaiye hain
+// function Hobbies(...hobbies){
+//     console.log(`I enjoy these Hobbies ${hobbies}`);
+// }
+
+// Hobbies("Coding" ,"Reading ","Cricket","Watch Facebok","Tik Toke","Movies");
+
+
+
+// Multiline Template Literals: Use template literals to create a multiline string that describes your ideal day. 
+// Include at least three different activities.
+
+// let myIdealDay=`My ideal day would Involve:
+// 1:Get up Early in the morning And Go to University
+// 2:Few hours to give coding and projects
+// 3:Before Sleeping I pray namaz `;
+// console.log(myIdealDay);
+
+
+// Refactoring to Arrow Functions: Take a simple function that calculates the area of a rectangle and 
+// refactor it into an arrow function.
+
+// function calculatArea(width,height){
+
+//     return width * height
+
+// }
+
+// let Arrow=(width,height)=> width*height
+// console.log(Arrow(5,10));
