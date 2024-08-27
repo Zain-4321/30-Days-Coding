63acd810e16b77b5c99e0ee61658df8957a9b532
ZAIN ALI SHAH <155235083+Zain-4321@users.noreply.github.com>
2024-08-28 00:08:52 +0500

    Day 19 array reduce method

---
+++ Script19.js
@@ -0,0 +1,31 @@
+// Day 19 WOrk on Java Script 
+
+
+// Double Numbers in an Array: Make a list of numbers. Then, use a trick to make a new list where each number is twice
+//  its original value.
+
+
+// let numbers=[1,2,3,4]
+// let doubleNumbers = numbers.map((number) => number * 2);
+// console.log(doubleNumbers)
+
+
+//Keep Only Strings: Given a mix of different types of items, make a new list that has only the words.
+
+
+// let mixarr=[1,"apple",5,"banana",7,"Orange"]
+// let stringArr=mixarr.filter((item)=>typeof item ==="string");
+// console.log(stringArr);
+
+
+
+
+//Find the Average Grade: Given a list of grades, calculate the average grade.
+
+
+// let grades =[88,98,78,75,65]
+
+// let averageGrade=grades.reduce((total,grad)=>total+grad,0)/grades.length;
+// console.log(averageGrade);
+//  grades.reduce((total, grade) => total + grade, 0) / grades.length;
+
