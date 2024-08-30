ef010d208c7d973479bb56f4ecff4ec5626a44a9
ZAIN ALI SHAH <155235083+Zain-4321@users.noreply.github.com>
2024-08-31 02:46:24 +0500

    DAY 28 uppercase ,lowercase ,Stringlenght ,replac

---
+++ Script28.js
@@ -0,0 +1,40 @@
+//DAY 28
+// Find the Length of a String: Write a function that takes a string as an argument and returns
+//  the number of characters in the string.
+
+// let String="ZAIN ALI SHAH";
+// console.log(String.length);
+// function myStr(str){
+//     return str.length;
+// }
+// console.log(myStr("Talha"));
+
+
+// Converting to Upper and Lower Case: Create a function that takes a string, converts it to 
+// uppercase, then to lowercase, and logs both.
+
+// function caseChange(str){
+//     let upperStr=str.toUpperCase();
+//     let lowerStr=str.toLowerCase();
+//     console.log("UperCaese:",upperStr ,"LowerCase:",lowerStr);
+
+// }
+
+// console.log(caseChange("Zain Ali Shah"));
+
+
+//Replacing Text in a String: Write a function that takes a sentence and replaces all
+// instances of the word `JavaScript` with `TypeScript`.
+// function replaceJavaScript(sentence) {
+//     return sentence.replace(/JavaScript/g, "TypeScript");
+// }
+
+// // Example usage:
+// let originalSentence = "JavaScript is a powerful language. Many developers use JavaScript.";
+// let updatedSentence = replaceJavaScript(originalSentence);
+
+// console.log(updatedSentence);
+
+// let name="ZAIN ALI SHAH";
+// let result=name.replace("ALI SHAH"," & TALHA");
+// console.log(result);
