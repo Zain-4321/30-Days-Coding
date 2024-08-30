97946f2c7595958179e9c62d6d335f4f7a27e0a4
ZAIN ALI SHAH <155235083+Zain-4321@users.noreply.github.com>
2024-08-31 02:24:17 +0500

    DAY 27 AGAIN OBJECT PRACTICE

---
+++ Script27.js
@@ -0,0 +1,59 @@
+//DAY 27 
+
+//Creating and Accessing Object Properties: Construct an object representing a car 
+// with properties for make, model, and year. Then, show how you can access the model
+//  property of the car.
+
+// let car={
+// make:"Toyota",
+// model:"Corolla 1.8",
+// year:2022,
+
+
+// }
+
+
+
+// for(let x in car){
+// console.log(`the car Companies make a car ${car[x]}`);
+// }
+
+
+// Updating Object Properties: Add a property named `color` to the existing car object, 
+// and then update the `year` property to `2021`. Show how to perform these operations.
+
+// let car={
+//     make:"Toyota",
+//     model:"corolla 1.8 grante",
+//     year:2020,
+
+
+// }
+// car.color="White";
+// car.NumberPlate=361738;
+
+// for(let x in car){
+//      console.log(`Car Details: \n ${car[x]}`);
+//      }
+    
+
+
+// Iterating Over Object Properties: Write a function that takes an object as an argument and 
+// logs all of its properties and values.
+
+// function objinFunc(obj){
+//     for(let property in obj){
+//         console.log(`Car Details ${property}: ${obj[property]}`);
+
+//     }
+// }
+// objinFunc({
+//     make:"TOYOTA",
+// model:"Corolla 1.8 Grante",
+// year:2022,
+// color:"White",
+// No_plate:361749,
+
+
+// });
+
