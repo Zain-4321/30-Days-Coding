0e17cd974a57bb622be609c6cd9b8392504a54bc
ZAIN ALI SHAH <155235083+Zain-4321@users.noreply.github.com>
2024-09-08 17:16:21 +0500

    Events Topic in JavaScript

--- App.js
+++ App.js
@@ -1,58 +1,61 @@
-// 08
+//EVENTS TODAY TPIC WE LEARN EVENTS
+// const bttn=document.querySelector(".btn");
+// console.log(bttn);
+//Second Method to use Events
+// bttn.onmouseover=function (){
+//     console.log("Learn JavaScript");
+// }
+//Third Type We Apply The Event
+//ADD EVEVT & EREMOVE EVENT
+//1) ADD EVENT THERE ARE THREE ARGUMENT
+                     //event name   functions call back boolean by default is false
+// bttn.addEventListener("click",()=>{
 
-// Intentional Error: If you haven’t received an array index error in one of your programs yet,
-//  try to make one happen. Change an index in one of your programs to produce an index error.
-//   Make sure you correct the error before closing the program.
+//     bttn.classList.add("btn1");
+// })
 
+//REMOVE
+// function click1(){
+//     console.log("Click 1")
+// }
+// function click2(){
+//     console.log("Click 2")
+// }
 
-// let fri=["Waqar","Salman","Talha"]
-//console.log(fri[3]);         so this is index errror
-// console.log(fri[2]);
-
-// Conditional Tests: Write a series of conditional tests. Print a statement describing each 
-// test and your prediction for the results of each test. Your code should look something like this, full-question
-
-//For true 
-
-// let test1="Apple"=="Apple";
-// console.log("Apple is equal to Apple that is True",test1);
-// let test2="Apple"!=="Orange";
-// console.log("Apple is Not  equal to Apple that is True",test2);
-// let test3=10>9;
-// console.log("10 is grater than 9 that is True",test3);
-// let test4=10>=10;
-// console.log("10 greater than  equal to 10 that is True",test4);
-// let test5 = 10 <= 10;
-// console.log("Is 10 less than or equal to 10? I predict True.",test5);
-
-// let fruits = ["apple", "banana", "cherry"];
-// let test7 = fruits.includes("banana");
-
-// let test7=fruits.includes("Orange")
-// console.log("Is 'banana' in the array of fruits? I predict True.");
-// console.log("Result:", test7);
-
-// More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more 
-// comparisons, write more tests. Have at least one True and one False result for each of the following, full-question
+// bttn.addEventListener("click",click1)
+// bttn.addEventListener("click",click2)
+// bttn.removeEventListener("click",click2)
 
 
-//               for string
-console.log("For String")
-console.log("Apple"=="Apple")
-console.log("Apple"=="apple")
 
-console.log("Testind With Uppecase")
-console.log("bmwcars".toLocaleUpperCase()=="BMWCARS");
+//-------------part 2--------------------------------------//
+//-----------Event Flow------------
+// const OutEle=document.querySelector(".outer");
+// const BtnEle=document.querySelector(".btn");
 
-// using operators
 
-console.log("Tests with 'and' and 'or':");
-console.log(true &&false);
-console.log(true || false);
+// OutEle.addEventListener("click",(e)=>{
+//     console.log("you click on div");
+//     e.stopPropagation()
+//     },true)
 
-let fruits=["banana","orange","Apple"]
-console.log(fruits.includes("graps"));
 
-// number
-console.log(50>40);
-console.log(5<4);
+// //false neechy se upper   
+// //true upper se neechy
+
+// BtnEle.addEventListener("click",(e)=>{
+// console.log("you click on button");
+// e.stopPropagation()
+// },true)
+
+
+
+//prevent 
+
+
+const GoogEle=document.getElementById("Google");
+
+GoogEle.addEventListener("click",(e)=>{
+    console.log("Click On GOOGLE BUTTON");
+    e.preventDefault()
+},false)
